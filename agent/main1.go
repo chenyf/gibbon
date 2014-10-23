eeb53c1328ec49010c6dbe89aa76cb4b1780c632
yumg <yumg@vm-10-154-156-121.(none)>
2014-10-23 15:08:58 +0800

    none

--- agent/main1.go
+++ agent/main1.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"github.com/widuu/gojson"
 	"io"
 	"io/ioutil"
-	"log"
+	log "github.com/cihub/seelog"
 	"net"
 	"net/http"
 	"os"
@@ -26,7 +26,7 @@ var Sequence uint32
 func main() {
 	/*
 	   if len(os.Args) != 2 {
-	           log.Printf("Usage : %s host:port",os.Args[0])
+	           log.Infof("Usage : %s host:port",os.Args[0])
 	           os.Exit(1)
 	   } */
 	//setLogOutput("./log.txt")
@@ -43,32 +43,42 @@ func clientProcess() {
 	var result uint8
 	var conn *net.TCPConn
 
-	err := LoadConfig("./conf.json")
+	err := LoadConfig("/system/etc/conf.json")
 	if err != nil {
-		log.Fatalf("LoadConfig failed: (%s)", err)
+		fmt.Printf("LoadConfig failed: (%s)", err)
+		os.Exit(1)
 	}
+
+	logger, err := log.LoggerFromConfigAsFile("/system/etc/log.xml")
+	if err != nil {
+		fmt.Printf("Load log config failed: (%s)\n", err)
+		os.Exit(1)
+	}
+	log.ReplaceLogger(logger)
+
 	var addSlice []string
 	addSlice = strings.Split(Config.Address, ";")
-	log.Printf("content: %s\n", addSlice)
+	log.Infof("content: %s\n", addSlice)
 	//var err os.Error
 	//service := os.Args[1]
 	//service := "10.58.65.221:9000"
 	for {
 		serverIpList := addSlice
 		for _, service := range serverIpList {
-			log.Printf("try to connect server address :%v\n", service)
+			log.Infof("try to connect server address :%v\n", service)
 			tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 			if err != nil {
-				log.Printf("resolve tcp address fail:%v\n", err)
+				log.Infof("resolve tcp address fail:%v\n", err)
 				continue
 			}
 
 			conn, err = net.DialTCP("tcp", nil, tcpAddr)
 			if err != nil {
-				log.Printf("conection fail:%v\n", err)
-				time.Sleep(10 * time.Second)
+				log.Infof("conection fail:%v\n", err)
+				time.Sleep(5 * time.Second)
 				continue
 			}
+			break
 		}
 		if conn != nil {
 			defer conn.Close()
@@ -76,7 +86,7 @@ func clientProcess() {
 			conn.SetNoDelay(true)
 			result = messgeProcess(conn)
 			if result == uint8(CLIENTERROR) {
-				log.Printf("reconnect server")
+				log.Infof("reconnect server")
 				continue
 			} else {
 				break
@@ -113,14 +123,25 @@ func (this *Packet) GetBytes() (buf []byte) {
 }
 
 func SendByteStream(conn *net.TCPConn, buf []byte) error {
-	log.Printf("send data header:%v", buf[0:10])
-	log.Printf("send data body:%s", string(buf[10:]))
+	log.Infof("send data header:%v", buf[0:10])
+	log.Infof("send data body:%s", string(buf[10:]))
 	n, err := conn.Write(buf)
 	if n != len(buf) || err != nil {
-		log.Printf("SendByteStream Write(): %v\n", err)
+		log.Infof("SendByteStream Write(): %v\n", err)
 		return err
 	}
 	return nil
+}
+
+
+func SendByteStreamKeepAlive(conn *net.TCPConn, buf []byte) error {
+        log.Infof("send data header:%v", buf[0:10])
+        n, err := conn.Write(buf[0:10])
+        if n != len(buf) || err != nil {
+                log.Infof("SendByteStream Write(): %v\n", err)
+                return err
+        }
+        return nil
 }
 
 type RegisterInfo struct {
@@ -138,15 +159,15 @@ func SendKeepAliveData(conn *net.TCPConn, dataType uint8) error {
 	buf = append(buf, Uint8ToBytes(pac.Ver)...)
 	buf = append(buf, Uint32ToBytes(pac.Seq)...)
 	buf = append(buf, Uint32ToBytes(pac.Len)...)
-	log.Printf("client will send keepalive message")
-	return SendByteStream(conn, buf)
+	log.Infof("client will send keepalive message")
+	return SendByteStreamKeepAlive(conn, buf)
 }
 
 func SendRegisterData(conn *net.TCPConn, dataType uint8) error {
 	var b []byte
 	macAddr, err := getMac()
 	if err != nil {
-		log.Printf("get mac addres failed:%v\n", err)
+		log.Infof("get mac addres failed:%v\n", err)
 	}
 	b = []byte(macAddr)
 	var pac Packet
@@ -155,7 +176,7 @@ func SendRegisterData(conn *net.TCPConn, dataType uint8) error {
 	pac.Ver = 88
 	pac.Len = uint32(len(b))
 	pac.Data = b
-	log.Printf("client will send register message")
+	log.Infof("client will send register message")
 	return SendByteStream(conn, pac.GetBytes())
 }
 
@@ -171,8 +192,8 @@ func SendResponseData(conn *net.TCPConn, dataType uint8, body []byte, bSeq uint3
 	pac.Len = uint32(len(body))
 	pac.Data = body
 
-	log.Printf("client will send response message")
-	log.Printf("send response body to server:%s\n", string(body))
+	log.Infof("client will send response message")
+	log.Infof("send response body to server:%s\n", string(body))
 	return SendByteStream(conn, pac.GetBytes())
 }
 
@@ -207,10 +228,10 @@ func KeepAlive(conn *net.TCPConn) uint8 {
 	for _ = range ticker.C {
 		err := SendKeepAliveData(conn, uint8(0))
 		if err != nil {
-			log.Printf("send keepalvie data fail:%v\r\n", err)
+			log.Infof("send keepalvie data fail:%v\r\n", err)
 			return uint8(CLIENTERROR)
 		}
-		log.Printf(conn.RemoteAddr().String(), "ping.")
+		log.Infof(conn.RemoteAddr().String(), "ping.")
 	}
 	return uint8(0)
 }
@@ -221,7 +242,7 @@ func messgeProcess(conn *net.TCPConn) uint8 {
 	//send register message
 	err := SendRegisterData(conn, uint8(1))
 	if err != nil {
-		log.Printf("send register data fail:%v\r\n", err)
+		log.Infof("send register data fail:%v\r\n", err)
 		// os.Exit(1)
 	}
 
@@ -238,20 +259,35 @@ func messgeProcess(conn *net.TCPConn) uint8 {
 		var result uint8
 		result = KeepAlive(conn)
 		if result == uint8(CLIENTERROR) {
-			log.Printf("quit message process result is:%d\n", result)
+			log.Infof("quit message process result is:%d\n", result)
 			c <- uint8(CLIENTERROR)
 		}
 		c <- uint8(0)
 	}()
+
+	/*client := &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				conn, err := net.DialTimeout(netw, addr, time.Second*5)
+				if err != nil {
+					return nil, err
+				}
+				conn.SetDeadline(time.Now().Add(time.Second * 2))
+				return conn, nil
+			},
+			ResponseHeaderTimeout: time.Second * 2,
+		},
+	}*/
 	// loop for receive message
 	for {
 		// read message type
+		//time.Sleep(5 * time.Second)
 		if n, err := io.ReadFull(conn, bType); err != nil && n != 1 {
-			log.Printf("Read message type failed: %v\r\n", err)
+			log.Infof("Read message type failed: %v\r\n", err)
 			select {
 			case v := <-c:
 				if v == uint8(CLIENTERROR) {
-					log.Printf("quit read message process result is:%d\n", v)
+					log.Infof("quit read message process result is:%d\n", v)
 					return uint8(CLIENTERROR)
 				} else {
 					break
@@ -260,68 +296,70 @@ func messgeProcess(conn *net.TCPConn) uint8 {
 			continue
 		}
 		messageType := BytesToUint8(bType)
-		log.Printf("clent receive message type %d", messageType)
+		log.Infof("clent receive message type %d", messageType)
 
 		switch messageType {
 		// receive register reponse
 		case 2:
 			if n, err := io.ReadFull(conn, bVer); err != nil && n != 1 {
-				log.Printf("Read register message ver failed: %v\r\n", err)
+				log.Infof("Read register message ver failed: %v\r\n", err)
 				continue
 			}
 
 			if n, err := io.ReadFull(conn, bSeq); err != nil && n != 4 {
-				log.Printf("Read register message seq failed: %v\r\n", err)
+				log.Infof("Read register message seq failed: %v\r\n", err)
 				continue
 			}
 
 			if n, err := io.ReadFull(conn, bLen); err != nil && n != 4 {
-				log.Printf("Read register message Len failed: %v\r\n", err)
+				log.Infof("Read register message Len failed: %v\r\n", err)
 				continue
 			}
 			pacLen = BytesToUint32(bLen)
 			pacData := make([]byte, pacLen)
 			if n, err := io.ReadFull(conn, pacData); err != nil && n != int(pacLen) {
-				log.Printf("Read register pacData failed: %v\r\n", err)
+				log.Infof("Read register pacData failed: %v\r\n", err)
 				continue
 			}
 
 			err = json.Unmarshal([]byte(pacData), &response)
 			if err != nil {
-				log.Printf("register json decode error: %v\r\n", err)
+				log.Infof("register json decode error: %v\r\n", err)
 				continue
 			}
 		//receive server request
 		case 3:
 			if n, err := io.ReadFull(conn, bVer); err != nil && n != 1 {
-				log.Printf("Read request message ver failed: %v\r\n", err)
+				log.Infof("Read request message ver failed: %v\r\n", err)
 				continue
 			}
 
 			if n, err := io.ReadFull(conn, bSeq); err != nil && n != 4 {
-				log.Printf("Read request message seq failed: %v\r\n", err)
+				log.Infof("Read request message seq failed: %v\r\n", err)
 				continue
 			}
 
 			if n, err := io.ReadFull(conn, bLen); err != nil && n != 4 {
-				log.Printf("Read request message Len failed: %v\r\n", err)
+				log.Infof("Read request message Len failed: %v\r\n", err)
 				continue
 			}
 			pacLen = BytesToUint32(bLen)
 			pacData := make([]byte, pacLen)
 			if n, err := io.ReadFull(conn, pacData); err != nil && n != int(pacLen) {
-				log.Printf("Read request pacData failed: %v\r\n", err)
+				log.Infof("Read request pacData failed: %v\r\n", err)
 				continue
 			}
 			// parse jason
 			uidinfo := gojson.Json(string(pacData)).Get("uid").Tostring()
-			log.Printf("uid info:%v", uidinfo)
+			log.Infof("uid info:%v", uidinfo)
 			cmdinfo := gojson.Json(string(pacData)).Get("cmd").Tostring()
-			log.Printf("cmd info:%v", cmdinfo)
+			log.Infof("cmd info:%v", cmdinfo)
+			request := gojson.Json(cmdinfo).Get("forward").Tostring()
+			log.Infof("request info:%v", request)
 			var responsebody []byte
 			if len(cmdinfo) != 0 {
-				log.Printf("enter cmd process branch\n")
-				requestbody := bytes.NewBuffer([]byte(cmdinfo))
+				log.Infof("enter cmd process branch\n")
+				requestbody := bytes.NewBuffer([]byte(request))
 
 				client := &http.Client{
 					Transport: &http.Transport{
@@ -330,25 +368,34 @@ func messgeProcess(conn *net.TCPConn) uint8 {
 							if err != nil {
 								return nil, err
 							}
-							conn.SetDeadline(time.Now().Add(time.Second * 10))
+							conn.SetDeadline(time.Now().Add(time.Second * 2))
 							return conn, nil
 						},
-						ResponseHeaderTimeout: time.Second * 10,
+						ResponseHeaderTimeout: time.Second * 2,
 					},
 				}
 
-				log.Printf("send http body:%v", cmdinfo)
+				log.Infof("send http body:%v", cmdinfo)
 				if res, err := client.Post("http://127.0.0.1:9999/", "application/json;charset=utf-8", requestbody); err != nil {
-					log.Printf("http post request failed - %v\r\n", err)
+					log.Infof("http post request failed - %v\r\n", err)
 				} else {
 					result, err2 := ioutil.ReadAll(res.Body)
+					log.Infof("res body value is :%s\n", res.Body)
 					if err2 != nil {
-						log.Printf("http response read failed - %v\r\n", err2)
+						log.Infof("http response read failed - %v\r\n", err2)
 					}
 					res.Body.Close()
-					log.Printf("post request result:%s\n", result)
+					log.Infof("post request result:%s\n", result)
 					responsebody = result
-					SendResponseData(conn, uint8(4), responsebody, BytesToUint32(bSeq))
+					cmdHttpResp := CommandHttpResponse{
+						Status: 0,
+						Result: string(responsebody),
+						Descr:  "",
+					}
+
+					finalResp, _ := json.Marshal(cmdHttpResp)
+					log.Infof("post final response:%s\n", string(finalResp))
+					SendResponseData(conn, uint8(4), finalResp, BytesToUint32(bSeq))
 				}
 
 			}
@@ -358,10 +405,16 @@ func messgeProcess(conn *net.TCPConn) uint8 {
 				}*/
 		//invalid meessage
 		default:
-			log.Printf("invalid message type %d", messageType)
+			log.Infof("invalid message type %d", messageType)
 		}
 	}
 	return uint8(0)
+}
+
+type CommandHttpResponse struct {
+	Status uint8  `json:"status"`
+	Result string `json:"result"`
+	Descr  string `json:"descr"`
 }
 
 func StreamToByte(stream io.Reader) []byte {
@@ -369,20 +422,20 @@ func StreamToByte(stream io.Reader) []byte {
 	buf.ReadFrom(stream)
 	return buf.Bytes()
 }
-
+/*
 func setLogOutput(filepath string) {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	logfile, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if err != nil {
-		log.Printf("%v\r\n", err)
+		log.Infof("%v\r\n", err)
 	}
 	log.SetOutput(logfile)
-}
+}*/
 
 func getMac() (string, error) {
 	interfaces, err := net.Interfaces()
 	if err != nil {
-		log.Printf("get mac addres failed:%v\r\n", err)
+		log.Infof("get mac addres failed:%v\r\n", err)
 	}
 	var macAddr string
 	var mac string
@@ -390,7 +443,7 @@ func getMac() (string, error) {
 		macAddr = inter.HardwareAddr.String()
 		if macAddr != "" {
 			mac = strings.Replace(macAddr, ":", "", -1)
-			log.Printf("mac address:%s", mac)
+			log.Infof("mac address:%s", mac)
 		}
 	}
 	return mac, err
